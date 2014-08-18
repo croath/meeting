6fb1deafbad3f549fe0c93ee6f78c181c3ee8def
croath <croathliu@gmail.com>
2014-08-18 15:22:53 +0800

    some typo or indentations.

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -9,8 +9,8 @@ class RoomsController < ApplicationController
 
   def status
     @room = Room.find(params[:id])
-     @day_orders = @room.orders.map{|order| order if order.start_time.to_s(format = :number)[0..7] == params[:day] &&
-        order.end_time.to_s(format = :time) > DateTime.now.to_s(format = :time)}.compact
+    @day_orders = @room.orders.map{|order| order if order.start_time.to_s(format = :number)[0..7] == params[:day] &&
+      order.end_time.to_s(format = :time) > DateTime.now.to_s(format = :time)}.compact
   end
 
   # GET /rooms/1
